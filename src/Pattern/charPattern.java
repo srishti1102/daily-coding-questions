b53e7e70de218c31443b42676090bf19dc8a74c1
srishti1102 <srishtiyadav30052004@gmail.com>
2024-11-22 19:07:01 +0530

    patters

--- src/Pattern/charPattern.java
+++ src/Pattern/charPattern.java
@@ -2,6 +2,13 @@ package Pattern;
 
 public class charPattern {
     public static void main(String[] args) {
-
+        char Character ='A';
+     for(int i=0;i<4;i++){
+         for(int j=0;j<=i;j++){
+             System.out.print(Character);
+             Character++;
+         }
+         System.out.println();
+     }
     }
 }
